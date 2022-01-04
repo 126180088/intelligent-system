24212f1a9021e3c4d297decf8c9222c464b62ba7
126180088 <770271967@qq.com>
2022-01-04 17:37:38 +0800

    登陆接口修改

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { Layout, Input, Icon, Form, Button, Divider, message, notification } from 'antd'
 import { withRouter } from 'react-router-dom'
-// import axios from '@/api'
-// import { API } from '@/api/config'
+import axios from '@/api'
+import { API } from '@/api/config'
 import '@/style/view-style/login.scss'
 
 class Login extends Component {
@@ -20,36 +20,42 @@ class Login extends Component {
         e.preventDefault()
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                // let { username, password } = values
-                // axios
-                //     .post(`${API}/login`, { username, password })
-                //     .then(res => {
-                //         if (res.data.code === 0) {
-                //             localStorage.setItem('user', JSON.stringify(res.data.data.user))
-                //             localStorage.setItem('token', res.data.data.token)
-                //             this.props.history.push('/')
-                //             message.success('登录成功!')
-                //         } else {
-                //             // 这里处理一些错误信息
-                //         }
-                //     })
-                //     .catch(err => {})
+                let { username, password } = values
+                axios
+                    .post(`${API}/Home/CreateToken`, { username, password })
+                    .then(res => {
+                        console.log(res)
 
-                // 这里可以做权限校验 模拟接口返回用户权限标识
-                switch (values.username) {
-                    case 'admin':
-                        values.auth = 0
-                        break
-                    default:
-                        values.auth = 1
-                }
+                        if (res.data.status === 0) {
+                            localStorage.setItem('user', JSON.stringify(values))
+                            localStorage.setItem('token', res.data.data.token)
+                            this.props.history.push('/')
+                            message.success('登录成功!')
+                        } else {
+                            console.log(res.data.message);
+                            message.success('登录失败!')
+                            // 这里处理一些错误信息
+                        }
+                    })
+                    .catch(err => {
+                        console.log(`请求错误：${err} + ${API}/Home/CreateToken`)
+                    })
 
-                localStorage.setItem('user', JSON.stringify(values))
-                this.enterLoading()
-                this.timer = setTimeout(() => {
-                    message.success('登录成功!')
-                    this.props.history.push('/')
-                }, 2000)
+                // // 这里可以做权限校验 模拟接口返回用户权限标识
+                // switch (values.username) {
+                //     case 'admin':
+                //         values.auth = 0
+                //         break
+                //     default:
+                //         values.auth = 1
+                // }
+
+                // localStorage.setItem('user', JSON.stringify(values))
+                // this.enterLoading()
+                // this.timer = setTimeout(() => {
+                //     message.success('登录成功!')
+                //     this.props.history.push('/')
+                // }, 2000)
             }
         })
     }
@@ -58,7 +64,7 @@ class Login extends Component {
         notification.open({
             message: '欢迎使用后台管理平台',
             duration: null,
-            description: '账号 admin(管理员) 其他(游客) 密码随意'
+            description: '账号 admin(管理员) 密码 123'
         })
     }
 
@@ -73,7 +79,7 @@ class Login extends Component {
             <Layout className='login animated fadeIn'>
                 <div className='model'>
                     <div className='login-form'>
-                        <h3>后台管理系统</h3>
+                        <h3>应届生就业信息管理平台</h3>
                         <Divider />
                         <Form onSubmit={this.handleSubmit}>
                             <Form.Item>
