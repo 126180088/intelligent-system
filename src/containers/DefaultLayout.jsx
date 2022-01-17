6e296e53a2bdf236c8a0fa40eb0325bdcec3adc9
126180088 <770271967@qq.com>
2022-01-17 17:34:08 +0800

    数据库连接登陆

--- src/containers/DefaultLayout.jsx
+++ src/containers/DefaultLayout.jsx
@@ -37,6 +37,13 @@ class DefaultLayout extends Component {
         this.props.history.push('/login')
         message.success('登出成功!')
     }
+
+    userEdit = () =>{
+        
+    }
+
+
+
     getMenu = menu => {
         let newMenu,
             auth = JSON.parse(localStorage.getItem('user')).auth
